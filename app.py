3806b23de61615e80129b5ed5b9396a14b1c965f
mixassio <mixail.bondarev@gmail.com>
2017-03-31 11:31:18 +0000

    qwery

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 import chardet, codecs
 import csv
-
+from sqlalchemy import func
 import config
 
 app = Flask(__name__)
@@ -14,8 +14,26 @@ db = SQLAlchemy(app)
 if __name__ == '__main__':
     from models import *
     db.create_all()
+    """ simple qwery to table with where
     
-    print(db.session.query(Cinema).filter_by(year_of_issue='1960'))
+    print(db.session.query(Cinema).filter_by(year_of_issue='1935').all())
+    for i in db.session.query(Cinema).filter_by(year_of_issue='1935').all():
+        print(i.title, i.year_of_issue)"""
+    
+    dir = db.session.query(Director).filter_by(name='Стэнли Кубрик').all()
+    for i in dir[0].cinemas.all():
+        print(i.year_of_issue, i.title)
+        
+    print(db.session.query(Cinema.director_id, func.count(Cinema)).group_by(Cinema.director_id))
+    
+"""
+Нужны запросы:
+1. Полный список фильмов с подставленными странами, жанрами и режисерами
+2. Список фильмов по режисеру, стране, году git и ПЕРИОДУ, множественный выбор
+3. Конструкции вида count(*) group_by
+4. 
+"""
+    
 
         
     
